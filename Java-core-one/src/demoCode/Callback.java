6336cc518c6df7b52bf67b15d465f71ed78e5af7
Stan-Zhang <kuaibu2017@163.com>
2024-11-13 21:48:11 +0800

    add code：Java core one demo Callback.java

--- Java-core-one/src/demoCode/Callback.java
+++ Java-core-one/src/demoCode/Callback.java
@@ -1,7 +1,5 @@
 package demoCode;
 // 定义一个回调接口
 public interface Callback {
-    void inComplete(String inMessage);
     void onComplete(String onMessage);
-    void offComplete(Integer ofMessage);
 }
--- Java-core-one/src/demoCode/Work.java
+++ Java-core-one/src/demoCode/Work.java
@@ -3,15 +3,18 @@ package demoCode;
 public class Work {
     public void doWork(Callback callback){
         System.out.println("Starting async work...==================" + Thread.currentThread().getName());
+
+
         //模拟执行一些任务
         new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("Doing some work in a separate thread...==============" + Thread.currentThread().getName());
+                System.out.println("Doing Some work in a separate Thread...==============" + Thread.currentThread().getName());
                 // 模拟执行一些任务
                 try {
-                    Thread.sleep(20000);  // 模拟耗时任务
+                    Thread.sleep(10000);  // 模拟耗时任务
                 } catch (InterruptedException e) {
+                    System.out.println("Errer message:"+ e.getMessage());
                     e.printStackTrace();
                 }
 
