4f4f5b67d26b5bce751f9d8ac08fc4f722489ba5
Stan-Zhang <kuaibu2017@163.com>
2024-12-08 22:25:54 +0800

    add code: update  code

--- Java-core-one/src/day9/interfaces/comparatorString/LengthComparator.java
+++ Java-core-one/src/day9/interfaces/comparatorString/LengthComparator.java
@@ -2,7 +2,7 @@ package day9.interfaces.comparatorString;
 
 import java.util.Comparator;
 
-public class LengthComparator implements Comparator<String>,,Cloneable {
+public class LengthComparator implements Comparator<String>,Comparable,Cloneable {
     @Override
     public int compare(String first, String second) {
         return first.length() - second.length();
