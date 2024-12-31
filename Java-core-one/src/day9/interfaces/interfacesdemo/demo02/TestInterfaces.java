d6acda3cc28af8983a0e3c369d8f5e533cee90ae
Stan-Zhang <kuaibu2017@163.com>
2025-01-01 00:33:56 +0800

    add code: java17 path code

--- Java-core-one/src/day9/interfaces/interfacesdemo/demo02/TestInterfaces.java
+++ Java-core-one/src/day9/interfaces/interfacesdemo/demo02/TestInterfaces.java
@@ -1,10 +1,15 @@
 package day9.interfaces.interfacesdemo.demo02;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class TestInterfaces {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Usbs usbs;
         Pcit pcit;
         pcit = new PcitImpl();
@@ -12,6 +17,8 @@ public class TestInterfaces {
         System.out.println(pc);
 
         Path ss1 = Paths.get("ss");
+        Path path = FileSystems.getDefault().getPath("logs", "access. log");
+        BufferedReader reader = Files. newBufferedReader(path, StandardCharsets. UTF_8);
 
         //java 11新特性 取代了Paths。get
         Path ss = Path.of("ss");
